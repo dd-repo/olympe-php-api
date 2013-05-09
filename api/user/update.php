3df46c3a2d0a465f1972257b4a93731af49499f6
Samuel Hassine <samuel.hassine@anotherservice.com>
2013-05-09 02:02:16 +0200

    Password min 6chars

--- api/user/update.php
+++ api/user/update.php
@@ -24,7 +24,7 @@ $a->addParam(array(
 	'name'=>array('pass', 'password', 'user_password', 'user_pass'),
 	'description'=>'The password of the user.',
 	'optional'=>true,
-	'minlength'=>3,
+	'minlength'=>6,
 	'maxlength'=>50,
 	'match'=>request::PHRASE|request::SPECIAL,
 	'action'=>true
