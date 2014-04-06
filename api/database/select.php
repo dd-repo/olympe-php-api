095f3bfa916fa06232dca033510bc0e48e841fba
Samuel Hassine <samuel.hassine@anotherservice.com>
2014-04-06 02:44:50 +0200

    Bugfix

--- api/database/select.php
+++ api/database/select.php
@@ -98,6 +98,8 @@ $a->setExecute(function() use ($a)
 	{
 		$sql = "SELECT storage_size FROM storages WHERE storage_path = '/databases/{$r['database_name']}'";
 		$storage = $GLOBALS['db']->query($sql);
+		$sql = "SELECT COUNT(database_name) as count FROM `databases` WHERE database_server = '{$r['database_server']}'";
+		$stats = $GLOBALS['db']->query($sql);
 		$sql = "SELECT COUNT(database_name) as count FROM `databases` WHERE database_server = 'sql.olympe.in'";
 		$sql1 = $GLOBALS['db']->query($sql);
 		$sql = "SELECT COUNT(database_name) as count FROM `databases` WHERE database_server = 'sql2.olympe.in'";
@@ -109,7 +111,7 @@ $a->setExecute(function() use ($a)
 		$d['server'] = $r['database_server'];
 		$d['size'] = $storage['storage_size'];
 		$d['user'] = array('id'=>$r['user_id'], 'name'=>$r['user_name']);
-		$d['stats'] = array('sql.olympe.in'=>$sql1['count'], 'sql1.olympe.in'=>$sql1['count'], 'sql2.olympe.in'=>$sql2['count']);
+		$d['stats'] = array('sql.olympe.in'=>$sql1['count'], 'sql1.olympe.in'=>$sql1['count'], 'sql2.olympe.in'=>$sql2['count'], "{$r['database_server']}" => $stats['count'] );
 		
 		$databases[] = $d;
 	}
