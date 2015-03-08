273f9eaf815cbb34f4260a8bb9cd8d3fa9570e56
Romain Perrone <ro.perrone1@gmail.com>
2015-03-08 09:29:14 +0100

    api

---
+++ api/install/index.php
@@ -0,0 +1,19 @@
+<?php
+
+if( !defined('PROPER_START') )
+{
+	header("HTTP/1.0 403 Forbidden");
+	exit;
+}
+
+$i = new index();
+$i->addAlias(array('install', 'setup'));
+$i->setDescription("Launch install on the remote directory.");
+$i->addEntry('insert', array('insert', 'create', 'add'));
+$i->addEntry('select', array('select', 'list', 'view', 'search'));
+$i->addEntry('update', array('update', 'change', 'rename', 'modify'));
+$i->addEntry('delete', array('delete', 'remove', 'del', 'destroy'));
+
+return $i;
+
+?>
---
+++ api/install/insert.php
@@ -0,0 +1,72 @@
+<?php
+
+if( !defined('PROPER_START') )
+{
+	header("HTTP/1.0 403 Forbidden");
+	exit;
+}
+
+$a = new action();
+$a->addAlias(array('create', 'add'));
+$a->setDescription("Start installation");
+$a->addGrant(array('ACCESS', 'SITE_INSERT'));
+$a->setReturn(array(array(
+	'url'=>'the URL to the backup'
+	)));
+$a->addParam(array(
+	'name'=>array('site', 'site_id', 'id'),
+	'description'=>'The name or id of the target site.',
+	'optional'=>false,
+	'minlength'=>0,
+	'maxlength'=>50,
+	'match'=>request::LOWER|request::NUMBER|request::PUNCT
+	));
+$a->addParam(array(
+	'name'=>array('user', 'user_name', 'username', 'login', 'user_id', 'uid'),
+	'description'=>'The name or id of the target user.',
+	'optional'=>true,
+	'minlength'=>0,
+	'maxlength'=>50,
+	'match'=>request::LOWER|request::NUMBER|request::PUNCT,
+	'action'=>false
+	));
+
+$a->setExecute(function() use ($a)
+{
+	// =================================
+	// CHECK AUTH
+	// =================================
+	$a->checkAuth();
+
+	// =================================
+	// GET PARAMETERS
+	// =================================
+	$site = $a->getParam('site');
+	$user = $a->getParam('user');
+		
+	// =================================
+	// GET USER DATA
+	// =================================
+	if( $user !== null )
+	{ 
+		$sql = "SELECT user_ldap, user_id FROM users u WHERE ".(is_numeric($user)?"u.user_id=".$user:"u.user_name = '".security::escape($user)."'");
+		$userdata = $GLOBALS['db']->query($sql);
+		if( $userdata == null || $userdata['user_ldap'] == null )
+			throw new ApiException("Unknown user", 412, "Unknown user : {$user}");
+	}
+	
+
+	
+}
+	
+	// =================================
+	// LOG ACTION
+	// =================================	
+	// logger::insert('backup/insert', $a->getParams(), $userdata['user_id']);
+	
+	responder::send(array('url'=> $GLOBALS['gearman']));
+});
+
+return $a;
+
+?>
