68b4683a3a52246adf350a1f683b3e9dd16c4da8
Romain Perrone <ro.perrone1@gmail.com>
2015-03-22 12:35:09 +0100

    install

--- api/install/insert.php
+++ api/install/insert.php
@@ -6,13 +6,12 @@ if( !defined('PROPER_START') )
 	exit;
 }
 
-
 $a = new action();
 $a->addAlias(array('create', 'add'));
 $a->setDescription("Start installation");
 $a->addGrant(array('ACCESS', 'INSTALL_INSERT'));
 $a->setReturn(array(array(
-	'url'=>'the URL to the backup'
+	'url'=>'the URL to the remote installation'
 	)));
 $a->addParam(array(
 	'name'=>array('site', 'site_id', 'id'),
@@ -23,61 +22,81 @@ $a->addParam(array(
 	'match'=>request::LOWER|request::NUMBER|request::PUNCT
 	));
 $a->addParam(array(
-	'name'=>array('user', 'user_name', 'username', 'login', 'user_id', 'uid'),
-	'description'=>'The name or id of the target user.',
-	'optional'=>true,
+	'name'=>array('user'),
+	'description'=>'The id of the user.',
+	'optional'=>false,
 	'minlength'=>0,
 	'maxlength'=>50,
-	'match'=>request::LOWER|request::NUMBER|request::PUNCT,
-	'action'=>false
+	'match'=>request::LOWER|request::NUMBER|request::PUNCT
+	));
+$a->addParam(array(
+	'name'=>array('path'),
+	'description'=>'The installation directory.',
+	'optional'=>true,
+	'minlength'=>0,
+	'maxlength'=>200,
+	'match'=>request::ALL
+	));
+$a->addParam(array(
+	'name'=>array('type', 'select'),
+	'description'=>'The CMS you want to install.',
+	'optional'=>false,
+	'minlength'=>0,
+	'maxlength'=>200,
+	'match'=>request::LOWER
+	));
+$a->addParam(array(
+	'name'=>array('type', 'select'),
+	'description'=>'The CMS you want to install.',
+	'optional'=>false,
+	'minlength'=>0,
+	'maxlength'=>200,
+	'match'=>request::LOWER
+	));
+$a->addParam(array(
+	'name'=>array('database', 'database_id'),
+	'description'=>'The database that will be used for the installation.',
+	'optional'=>true,
+	'minlength'=>0,
+	'maxlength'=>200,
+	'match'=>request::ALL
 	));
 	
 $a->setExecute(function() use ($a)
 {
-
 	// =================================
 	// CHECK AUTH
 	// =================================
 	$a->checkAuth();
+	$site = $a->getParam('site');
+	$path = $a->getParam('path');
+	$type = $a->getParam('type');
+	$config = $a->getParam('database');
 	$user = $a->getParam('user');
 	
-	$sql = "SELECT user_ldap, user_id, user_name FROM users WHERE user_id=".$user;
+	$sql = "SELECT * FROM users WHERE user_id = {$user}"; 
 	$userdata = $GLOBALS['db']->query($sql);
 	
-	if( $userdata == null || $userdata['user_ldap'] == null )
-		throw new ApiException("Unknown user", 412, "Unknown user : {$user}");
+	if( !$userdata )
+		throw new ApiException("Unknown user", 412, "User {$user} does not exist");
 	
-	$user_dn = $GLOBALS['ldap']->getDNfromUID($userdata['user_ldap']);
-	$user_info = $GLOBALS['ldap']->read($user_dn);
-	$data = $user_info;
-	$command = "mkdir -p {$data['homeDirectory']} && chown {$data['uidNumber']}:33 {$data['homeDirectory']} && chmod 750 {$data['homeDirectory']}";
+	$dn = $GLOBALS['ldap']->getDNfromUID($site);
+	$data = $GLOBALS['ldap']->read($dn);
+	
+	$data['owner'] = explode( ',', explode ( '=', $data['owner'] )[1] )[0];
+	
+	if( !$data['uid'] || $data['owner'] != $userdata['user_name'] )
+		throw new ApiException("Forbidden", 403, "User {$user} ({$userdata['user_ldap']}) does not match owner of the site {$site}");
+	
+	$command[] = "cp -Rf /dns/in/olympe/on/import/{$type}-en_EN.zip  {$data['homeDirectory']}{$path}/file.zip";
+	// $GLOBALS['gearman']->sendAsync($command);	
+	
+	$command[] = "cd {$data['homeDirectory']}{$path} && unzip file.zip";
+	$result['command'] = $command;
+	
 	
 	
 	// TODO: Refactor all installations using a real shell script
-	$result['command'] = $command;
-	$command = "ls -alh {$data['homeDirectory']}";
-	$result['install'] = exec ( 'pecl install ssh2 http://pecl.php.net/get/ssh2-0.12.tgz' );
-	
-
-	$result['exists'] =  function_exists ( 'ssh2_exec' );
-	$result['me'] = exec('ls -al ~/.ssh'); 
-	/*
-	// =================================
-	// GET PARAMETERS
-	// =================================
-	$site = $a->getParam('site');
-	$user = $a->getParam('user');
-		
-	// =================================
-	// GET USER DATA
-	// =================================
-	if( $user !== null )
-	{ 
-		$sql = "SELECT user_ldap, user_id FROM users u WHERE ".(is_numeric($user)?"u.user_id=".$user:"u.user_name = '".security::escape($user)."'");
-		$userdata = $GLOBALS['db']->query($sql);
-		if( $userdata == null || $userdata['user_ldap'] == null )
-			throw new ApiException("Unknown user", 412, "Unknown user : {$user}");
-	}*/
 	responder::send($result);
 }
 	
